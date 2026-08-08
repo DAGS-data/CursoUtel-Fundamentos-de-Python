6898175ddf209b8ec3fc23d6d91a2803eb3fa5aa
Diego-galsan <diego.galvan.sandoval@gmail.com>
2026-08-07 19:59:07 -0600

    Se suben clases 4 y 5

---
+++ Clase_5.py
@@ -0,0 +1,21 @@
+
+
+# if - else
+ 
+print("LLegas a una nueva ciudad-")
+
+viajar_1 = input("Deseas viajar a otra ciudad? (True/False): ")
+viajar_2 = input("Deseas viajar a la segunda ciudad? (True/False): ")
+viajar_3 = input("Deseas viajar a la tercera ciudad? (True/False): ")
+
+
+if viajar_1 == "True":
+    print("Continua a la siguiente ciudad 1.")
+elif viajar_2 == "True":
+   print("Continua a la siguiente ciudad 2.")
+elif viajar_3 == "True":
+    print("Continua a la siguiente ciudad 3.")
+else: 
+    print("Gracias por tu visita, regresa pronto.")
+
+
